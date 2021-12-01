b7552588a92dc67b2740ff79e52be2b369fb0722
Leon <silenceace@gmail.com>
2021-12-01 15:17:35 +0800

    chore: page

--- src/pages/index/views/Home.vue
+++ src/pages/index/views/Home.vue
@@ -1,46 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo"
-         src="@/assets/logo.png" />
-    {{hello}}
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    {{apiResponseString}}_{{currentStatus}}
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+    </ul>
   </div>
-</template>
-
-<script lang="ts">
-import Component, { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import AxiosRequest from '@/utils/request';
-import { AxiosResponse } from 'axios';
-import store from '@/store';
-
-const api: AxiosRequest = new AxiosRequest({
-  baseURL: 'https://reqbin.com/echo/post/json'
-});
-@Options({
-  components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {
-  apiResponseString: string = '';
-  currentStatus: string = store.getters.status;
-
-  apiDemo() {
-    api.get('').then((res: AxiosResponse<any, any>) => {
-      this.apiResponseString = JSON.stringify(res);
-    });
-  }
-
-  mounted() {
-    this.apiDemo();
-  }
-
-  data() {
-    return {
-      hello: 'hello world'
-    };
-  }
-}
-</script>
+</template>;
