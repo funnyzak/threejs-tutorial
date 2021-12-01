5a8fc07ff15243ae5ae094670a7241c834cc64ff
Leon <silenceace@gmail.com>
2021-12-01 15:03:56 +0800

    chore: route

--- src/pages/index/router/index.ts
+++ src/pages/index/router/index.ts
@@ -1,11 +1,30 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
+import TutorialHello from '../views/tutorial/Hello.vue';
+import TutoialSplit from '../views/tutorial/Split.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/tutorial',
+    name: '案例',
+    redirect: '/tutorial/hello',
+    children: [
+      {
+        path: '/hello',
+        name: 'TutorialHello',
+        component: TutorialHello
+      },
+      {
+        path: '/split',
+        name: 'TutorialSplit',
+        component: TutoialSplit
+      }
+    ]
   }
 ];
 
